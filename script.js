de2a50511f300d23fd25c3cba4dcdec11cb644c7
Jacob Handelman <jacobhandelman@gmail.com>
2022-03-21 13:30:44 -0400

    grid now works properly i.e 16x16 = evenly shaped square divs to work with

--- script.js
+++ script.js
@@ -4,40 +4,47 @@ const content = document.createElement('div');
     content.classList.add('content');
     container.appendChild(content);
 
-// creats multiple divs and assigns them a class. -- NEED TO ADD STYLES AND NAME THE CLASS TOO!
+// creats multiple divs and assigns them a class.
 function divMake(numOf){
   
   for (let i = 0; i < numOf; i++){
-    
     let div = document.createElement('div');
       content.appendChild(div);
-      div.classList.add(`square${i}`);
-      x = `square${i}`;
+      let divSelect = `square${i}`
+      div.classList.add(divSelect);
+      div.style.display = "flex" ;
       div.style.flex = "1 0 auto";
-      div.style.background = "black";
-      div.style.display = "flex";
       div.style.flexDirection = "row";
+
+    for (let j = 0; j < numOf; j++){                // creates n divs to make grid of equal squares.
+      let divInDiv = document.createElement('div');
+      div.appendChild(divInDiv);
+      divInDiv.classList.add('divColor')
+      divInDiv.style.background = "black";
+      divInDiv.style.flex = "1 0 auto";
+      
+    }
+    
   }
+  
   
 
     
 }
 
 
-//function classCreate(numOf){ 
-  //for (let i =0; i < numOf; i++){
-    
-   // divMake(`square${i}`, numOf);
-    
-    //}
   
-//}
+  
+
+
+
+
+
 
   
 
-const testNum = 6;
+const testNum = 50;
 divMake(testNum)
 
-//square.style.width = `${960 / gridAmount}px`;
-//square.style.height = `${500 / gridAmount}px`;
+
 
